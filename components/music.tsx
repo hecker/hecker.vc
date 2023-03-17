26893bb80b63e35705fa085aa02416a0328945b9
Jan Hecker <23746655+janhecker@users.noreply.github.com>
2023-03-17 18:07:47 +0100

    Use background music with HTMLAudioElement instead of external library.

--- components/music.tsx
+++ components/music.tsx
@@ -5,30 +5,27 @@ import { useEffect, useState, useRef } from "react";
 function BackgroundMusicOnL() {
   const [isPlaying, setIsPlaying] = useState(false);
   const url = "/music/last-of-the-real-ones.mp3";
-  // @ts-ignore
-  // eslint-disable-next-line
-  const audio = useRef(new Audio(url));
+  const audio = useRef<HTMLAudioElement | undefined>(
+    typeof Audio !== "undefined" ? new Audio(url) : undefined
+  );
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "l") {
         if (!isPlaying) {
-          audio.current.play();
+          audio.current?.play();
           setIsPlaying(true);
         } else {
-          audio.current.pause();
+          audio.current?.pause();
           setIsPlaying(false);
         }
       }
     };
-
     document.addEventListener("keydown", handleKeyDown);
-
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
   }, [isPlaying]);
-
   return null;
 }
 
