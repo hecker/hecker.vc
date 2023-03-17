71525f27b0338cdafbba743ce062702653704156
Jan Hecker <23746655+janhecker@users.noreply.github.com>
2023-03-17 03:42:29 +0100

    Change media security roles in order to play music.

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const ContentSecurityPolicy = `
     script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
     style-src 'self' 'unsafe-inline';
     img-src * blob: data:;
-    media-src 'none';
+    media-src 'self' 'unsafe-eval' 'unsafe-inline' vercel.live;
     connect-src *;
     font-src 'self';
 `;
