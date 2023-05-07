83489f587d6a4bdd7da9f6777cd8497b23fae445
Jan Hecker <23746655+janhecker@users.noreply.github.com>
2023-05-07 16:21:22 +0200

    Add Jodel part to /about.

--- app/(navbar)/about/page.tsx
+++ app/(navbar)/about/page.tsx
@@ -15,24 +15,18 @@ export default function AboutPage() {
           I started programming when I was around the age of 14. Back then, I
           had my own Minecraft server with a small team, website, web shop and
           some creative maps and game modes, which I developed by myself and
-          with the help of{" "}
-          <a
-            href="https://github.com/hannibal002"
-            style={{ fontWeight: "normal" }}
-          >
-            Lorenz
-          </a>
-          , who was my first mentor and taught me all the basics in Java. I
+          with the help of <a href="https://github.com/hannibal002">Lorenz</a>,
+          who was my first mentor and taught me all the basics in Java. I
           noticed that{" "}
           <b>I loved coding, building stuff and creating online communities.</b>
         </p>
         <p>
           During that time I had the idea to start a game server hosting
           business but decided not to because I was under the age of 18, PayPal
-          would't allow me to create a business account (even if I legally owned
-          a business), and my parents told me that school was too important and
-          that I should wait until I finished my A-Levels. In retrospect, I very
-          much regret not having founded it then.
+          wouldn't allow me to create a business account (even if I legally
+          owned a business), and my parents told me that school was too
+          important and that I should wait until I finished my A-Levels. In
+          retrospect, I very much regret not having founded it then.
         </p>
         <p>
           During my senior year of high school, I got to start{" "}
@@ -44,12 +38,20 @@ export default function AboutPage() {
           social network vamos! so that young people can connect with each other
           and see what's going on in the area.
         </p>
+        <p>
+          This era ended in 2023 after we got acquired by{" "}
+          <a href="https://jodel.com/de/">Jodel</a>, another social network from
+          Berlin, Germany, but with significantly more users, funding, and
+          experience. Since then, I've been working on product and moderation at
+          Jodel.
+        </p>
         <p className="mb-8">
           If you want to follow my journey or just want to grab a (virtual)
-          coffee, feel free to connect with me!
+          coffee, feel free to <a href="socials">connect with me</a>!
         </p>
 
-        <div className="flex flex-col gap-2 md:flex-row md:gap-2">
+        {/* <div className="flex flex-col gap-2 md:flex-row md:gap-2"> */}
+        <div className="hidden">
           <a
             rel="noopener noreferrer"
             target="_blank"
