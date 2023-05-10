13cd6337f17d4041662e8ed4bc44c4d975ac72da
Jan Hecker <23746655+janhecker@users.noreply.github.com>
2023-05-10 23:27:51 +0200

    Add green indicator where I'm most active.

--- app/(navbar)/socials/page.tsx
+++ app/(navbar)/socials/page.tsx
@@ -16,15 +16,18 @@ import {
   StravaIcon,
   JodelIcon,
 } from "components/icons";
+import Link from "next/link";
 
-function Link({
+function SocialLink({
   href,
   icon,
   title,
+  active,
 }: {
   href: string;
   icon: JSX.Element;
   title: string;
+  active: boolean;
 }) {
   return (
     <a
@@ -37,6 +40,12 @@ function Link({
         {icon}
         <div className="ml-3">{title}</div>
       </div>
+      {active && (
+        <span className="relative flex h-3 w-3">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+        </span>
+      )}
     </a>
   );
 }
@@ -49,91 +58,118 @@ export const metadata: Metadata = {
 export default function SocialsPage() {
   return (
     <section>
-      <h1 className="font-bold text-3xl font-serif">Socials</h1>
-      <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200">
-        <p>
-          I'm most active on LinkedIn. But don't you dare not follow me on every
-          single channel. ;) All icons are from{" "}
-          <a href="https://icons8.com/">Icons8</a>.
-        </p>
-      </div>
+      <h1 className="font-bold text-3xl font-serif mb-4">Socials</h1>
       <div className="grid gap-2 grid-cols-1 md:grid-cols-3">
-        <Link
+        <SocialLink
           href="https://github.com/hecker"
           icon={<GitHubIcon />}
           title="GitHub"
+          active={true}
         />
-        <Link
+        <SocialLink
           href="https://www.youtube.com/@janhecker00"
           icon={<YouTubeIcon />}
           title="YouTube"
+          active={true}
         />
-        <Link
-          href="https://www.twitch.tv/hecker"
-          icon={<TwitchIcon />}
-          title="Twitch"
-        />
-        <Link
-          href="https://www.polywork.com/hecker"
-          icon={<PolyworkIcon />}
-          title="Polywork"
-        />
-        <Link
+        <SocialLink
           href="https://www.linkedin.com/in/janhecker/"
           icon={<LinkedInIcon />}
           title="LinkedIn"
+          active={true}
         />
-        <Link
-          href="https://twitter.com/janhecker29"
-          icon={<TwitterIcon />}
-          title="Twitter"
-        />
-        <Link
-          href="https://open.spotify.com/user/eja8fqoy7qpqdm4bk7e5nt8o3?si=43929b476d604ad4"
-          icon={<SpotifyIcon />}
-          title="Spotify"
-        />
-        <Link
+        <SocialLink
           href="https://www.tiktok.com/@janhecker"
           icon={<TikTokIcon />}
           title="TikTok"
+          active={true}
         />
-        <Link
+        <SocialLink
           href="https://www.instagram.com/janhecker00/"
           icon={<InstagramIcon />}
           title="Instagram"
+          active={true}
         />
-        <Link
+        <SocialLink
+          href="https://open.spotify.com/user/eja8fqoy7qpqdm4bk7e5nt8o3?si=43929b476d604ad4"
+          icon={<SpotifyIcon />}
+          title="Spotify"
+          active={false}
+        />
+        <SocialLink
           href="https://bere.al/janhecker"
           icon={<BeRealIcon />}
           title="BeReal"
+          active={false}
         />
-        <Link
+        <SocialLink
           // Save for later: https://discordapp.com/users/220992532836777985
           href="https://discord.gg/YYuraFejJX"
           icon={<DiscordIcon />}
           title="Discord"
+          active={false}
         />
-        <Link
+        <SocialLink
           href="https://chat.whatsapp.com/LhFGMUMgsylHwZF5Irfh8l"
           icon={<WhatsAppIcon />}
           title="WhatsApp"
+          active={false}
         />
-        <Link
+        <SocialLink
           href="https://link.clashroyale.com/invite/friend/de?tag=2YJCG20LL&token=8myh8tjx&platform=iOS"
           icon={<ClashRoyaleIcon />}
           title="Clash Royale"
+          active={false}
         />
-        <Link
+        <SocialLink
           href="https://www.strava.com/athletes/janhecker"
           icon={<StravaIcon />}
           title="Strava"
+          active={false}
         />
-        <Link
+        <SocialLink
           href="https://shared.jodel.com/3rxqoI9ixzb"
           icon={<JodelIcon />}
           title="Jodel"
+          active={false}
         />
+        <SocialLink
+          href="https://www.twitch.tv/hecker"
+          icon={<TwitchIcon />}
+          title="Twitch"
+          active={false}
+        />
+        <SocialLink
+          href="https://www.polywork.com/hecker"
+          icon={<PolyworkIcon />}
+          title="Polywork"
+          active={false}
+        />
+        <SocialLink
+          href="https://twitter.com/janhecker29"
+          icon={<TwitterIcon />}
+          title="Twitter"
+          active={false}
+        />
+      </div>
+      <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200">
+        <p>
+          I'm most active on LinkedIn. But don't you dare not follow me on every
+          single channel. ;){"  "}
+          <span className="inline-flex items-center">
+            <span className="relative flex h-3 w-3">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+            </span>
+          </span>
+          {"  "}
+          indicates that I am active and publicly available on the platform. All
+          icons are from{" "}
+          <Link className="font-normal no-underline" href="https://icons8.com/">
+            Icons8
+          </Link>
+          .
+        </p>
       </div>
     </section>
   );
